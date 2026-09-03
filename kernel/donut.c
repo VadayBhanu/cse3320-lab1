246a12422f74d1b94b191b8d3de38e5623b89de5
Bhanu Yadav <102874413+VadayBhanu@users.noreply.github.com>
2026-09-02 23:10:07 -0500

    quest10: implement pixel donut timer animation
    Implemented the Quest 10 hardware timer re-arm for the pixel donut animation.

--- kernel/donut.c
+++ kernel/donut.c
@@ -77,40 +77,46 @@ static int draw_frame(TKernelTimerHandle hTimer, void *param, void *context) {
                 y = 12 + 15 * (cB * x4 + sB * x1) / x6,
                 // N = (((-cA * x7 - cB * ((-sA * x7 >> 10) + x2) - ci * (cj * sB >> 10)) >> 10) - x5) >> 7;
                 lumince = (((-cA * x7 - cB * ((-sA * x7 >> 10) + x2) - ci * (cj * sB >> 10)) >> 10) - x5); 
-                // range likely: <0..~1408, scale to 0..255
-                lumince = lumince<0? 0 : lumince/5; 
-                lumince = lumince<255? lumince : 255; 
+
+            // range likely: <0..~1408, scale to 0..255
+            lumince = lumince < 0 ? 0 : lumince / 5; 
+            lumince = lumince < 255 ? lumince : 255; 
 
             int o = x + 80 * y; // fxl: 80 chars per row
             signed char zz = (x6 - K2) >> 15;
-            if (22 > y && y > 0 && x > 0 && 80 > x && zz < z[o]) { // fxl: z depth will control visibility
+
+            if (22 > y && y > 0 && x > 0 && 80 > x && zz < z[o]) {
                 z[o] = zz;
-                // luminance_index is now in the range 0..11 (8*sqrt(2) = 11.3)
-                // now we lookup the character corresponding to the
-                // luminance and plot it in our output:
-                // b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
                 b[o] = lumince;                    
             }
+
             R(5, 8, ci, si) // rotate i
         }
+
         R(9, 7, cj, sj) // rotate j
     }
+
     R(5, 7, cA, sA);
     R(5, 8, cB, sB);
 
     int y = 0, x = 0;
+
     for (int k = 0; 1761 > k; k++) {
         if (k % 80) {
             if (x < 50) {
-                // to display, scale x by K, y by 2K (so we have a round donut)
-                int K=6, xx=x*K, yy=y*K*2;
-                // PIXEL clr = b[k]; // blue only, simple
-                PIXEL clr = int2rgb(b[k]); // to a color spectrum
+                // to display, scale x by K, y by 2K
+                int K = 6;
+                int xx = x * K;
+                int yy = y * K * 2;
+
+                PIXEL clr = int2rgb(b[k]);
+
                 setpixel(the_fb.fb, xx, yy, the_fb.pitch, clr);
-                setpixel(the_fb.fb, xx+1, yy, the_fb.pitch, clr);
-                setpixel(the_fb.fb, xx, yy+1, the_fb.pitch, clr);
-                setpixel(the_fb.fb, xx+1, yy+1, the_fb.pitch, clr);
+                setpixel(the_fb.fb, xx + 1, yy, the_fb.pitch, clr);
+                setpixel(the_fb.fb, xx, yy + 1, the_fb.pitch, clr);
+                setpixel(the_fb.fb, xx + 1, yy + 1, the_fb.pitch, clr);
             }
+
             x++;
         } else { 
             y++;
@@ -123,43 +129,54 @@ static int draw_frame(TKernelTimerHandle hTimer, void *param, void *context) {
 
 void donut(void) {
     int ret; 
+
     canvas_init();
-    ret = ktimer_start(100, /*firing interval, ms*/
-        draw_frame, 0 /*args*/, 0 /* context */); 
-    BUG_ON(ret<0);     
+
+    ret = ktimer_start(
+        100,                /* firing interval, ms */
+        draw_frame,
+        0,                  /* args */
+        0                   /* context */
+    ); 
+
+    BUG_ON(ret < 0);     
 }
 
 /* simple way to drive animation, NOT using vtimer. just hw irq 
-    to enable it,  irq handler must be modified to call sys_timer_irq_simple()    
+    to enable it, irq handler must be modified to call
+    sys_timer_irq_simple()
 */
 void donut_simple(void) {
     canvas_init();
-    put32(TIMER_C1, 100 * 1000);	// in us
+    put32(TIMER_C1, 100 * 1000);    // in us
 }
 
 // // copied from timer.c. dirty. just to verify that this works
 // static inline unsigned long current_counter() {
-// 	// assume these two are consistent, since the clock is only 1MHz...
-// 	return ((unsigned long) get32(TIMER_CHI) << 32) | get32(TIMER_CLO); 
+//     // assume these two are consistent, since the clock is only 1MHz...
+//     return ((unsigned long) get32(TIMER_CHI) << 32) | get32(TIMER_CLO); 
 // }
 
 extern unsigned long current_counter(); // timer.c, dirty. 
 
-//quest: Pixel donut. 
+// quest: Pixel donut.
 void sys_timer_irq_simple(void) 
 {
-	unsigned long cur; 
-    BUG_ON(!(get32(TIMER_CS) & TIMER_CS_M1));  
-	put32(TIMER_CS, TIMER_CS_M1);	// clear timer1 match
-    draw_frame(0, 0, 0); 
-    cur = current_counter(); 
+    unsigned long cur;
+
+    BUG_ON(!(get32(TIMER_CS) & TIMER_CS_M1));
+
+    put32(TIMER_CS, TIMER_CS_M1);    // clear timer1 match
+
+    draw_frame(0, 0, 0);
+
+    cur = current_counter();
+
     // reset the timer to fire in the future
-// 	put32(TIMER_C1, cur + 100 * 1000 /*in us*/);	//!STUDENT_DONOT_SEE 
-	/* STUDENT_TODO: your code here */
+    put32(TIMER_C1, cur + 100 * 1000);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-
 
 /* same as above, but print chars to uart. use delay() -- inefficient
     qemu: ok
@@ -171,9 +188,12 @@ void donut_text(void) {
     while (1) {
         memset(b, 32, 1760);  // text buffer
         memset(z, 127, 1760); // z buffer
+
         int sj = 0, cj = 1024;
+
         for (int j = 0; j < 90; j++) {
             int si = 0, ci = 1024; // sine and cosine of angle i
+
             for (int i = 0; i < 324; i++) {
                 int R1 = 1, R2 = 2048, K2 = 5120 * 1024;
 
@@ -187,56 +207,62 @@ void donut_text(void) {
                     x7 = cj * si >> 10,
                     x = 25 + 30 * (cB * x1 - sB * x4) / x6,
                     y = 12 + 15 * (cB * x4 + sB * x1) / x6,
-                    N = (((-cA * x7 - cB * ((-sA * x7 >> 10) + x2) - ci * (cj * sB >> 10)) >> 10) - x5) >> 7;
+                    N = (((-cA * x7
+                        - cB * ((-sA * x7 >> 10) + x2)
+                        - ci * (cj * sB >> 10)) >> 10)
+                        - x5) >> 7;
 
-                int o = x + 80 * y; // fxl: 80 chars per row
+                int o = x + 80 * y;
                 signed char zz = (x6 - K2) >> 15;
+
                 if (22 > y && y > 0 && x > 0 && 80 > x && zz < z[o]) {
                     z[o] = zz;
-                    // quest: change luminance of Donut 
-                    // luminance_index is now in the range 0..11 (8*sqrt(2) = 11.3)
-                    // now we lookup the character corresponding to the
-                    // luminance and plot it in our output:
+
                     b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
                 }
+
                 R(5, 8, ci, si) // rotate i
             }
+
             R(9, 7, cj, sj) // rotate j
         }
+
         R(5, 7, cA, sA);
         R(5, 8, cB, sB);
 
         for (int k = 0; 1761 > k; k++)
             putc(0, k % 80 ? b[k] : 10);
-        printf("\x1b[23A");  // clear console 
-        ms_delay(10);  // can delay in this way, but inefficient
+
+        printf("\x1b[23A");
+        ms_delay(10);
     }
 }
 
 // map luminance [0..255] to rgb color
 // value: 0..255, PIXEL: argb
-static PIXEL int2rgb (int value) {
-    int r,g,b;     
+static PIXEL int2rgb(int value) {
+    int r, g, b;
+
     if (value >= 0 && value <= 85) {
-        // Black to Yellow (R stays 0, G increases, B stays 0)
         r = 0;
-        g = (value * 3);
+        g = value * 3;
         b = 0;
+
     } else if (value > 85 && value <= 170) {
-        // Yellow to Cyan (G stays 255, R decreases, B increases)
         r = 255 - ((value - 85) * 3);
         g = 255;
         b = (value - 85) * 3;
+
     } else if (value > 170 && value <= 255) {
-        // Cyan to Blue (G decreases, B stays 255, R stays 0)
         r = 0;
         g = 255 - ((value - 170) * 3);
         b = 255;
+
     } else {
-        // Value out of range
-        r=g=b=0;
-    }    
-    return (r<<16)|(g<<8)|b; 
+        r = g = b = 0;
+    }
+
+    return (r << 16) | (g << 8) | b;
 }
 
 /// project idea: use ktimer to drive the animation. periodically
@@ -253,12 +279,10 @@ static PIXEL int2rgb (int value) {
  * 2021-01-13     Andy Sloane  Third version
  * 2021-03-25     Meco Man     Port to RT-Thread RTOS
  *
+ * js code for both canvas & text version
+ * https://www.a1k0n.net/js/donut.js
  *
- *  js code for both canvas & text version
- *  https://www.a1k0n.net/js/donut.js
- *
- *  ported by FL
+ * ported by FL
  * From the NJU OS project:
  * https://github.com/NJU-ProjectN/am-kernels/blob/master/kernels/demo/src/donut/donut.c
- *
  */
