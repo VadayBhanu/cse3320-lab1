a25e9f3f89720a51a473506b171c5e7a332abf74
Bhanu Yadav <102874413+VadayBhanu@users.noreply.github.com>
2026-09-02 22:58:28 -0500

    quest09: enable CPU interrupts

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -16,36 +16,36 @@ extern void donut_text();      // donut.c
 
 void uart_send_string(char* str);
 
-struct cpu cpus[NCPU]; 
+struct cpu cpus[NCPU];
 
 void kernel_main() {
 	uart_init();                       // bring up the UART for kernel debugging
-	init_printf(NULL, putc);          // wire printf() to output via UART (putc)
+	init_printf(NULL, putc);           // wire printf() to output via UART (putc)
 	printf("------ kernel boot ------  core %d\n\r", cpuid());
-	printf("build time (kernel.c) %s %s\n", __DATE__, __TIME__); // simplicity 
+	printf("build time (kernel.c) %s %s\n", __DATE__, __TIME__);
 
-	sys_timer_init();                   // kernel timer: delay, timekeeping...
-	enable_interrupt_controller(0);     // coreid
-	// quest: sys_timer irq
-// 	enable_irq();		// !STUDENT_DONOT_SEE
-	/* STUDENT_TODO: your code here */
+	sys_timer_init();                  // kernel timer: delay, timekeeping...
+	enable_interrupt_controller(0);    // coreid
 
-	generic_timer_init();               // periodic ticks alive
+	// Quest 09: enable CPU IRQ
+	enable_irq();
 
-	if (fb_init() != 0) BUG();          // will show the OS logo
+	generic_timer_init();              // periodic ticks alive
+
+	if (fb_init() != 0) BUG();         // will show the OS logo
 
 	// test_ktimer();
-	test_fb_voffset();                  // cycle through color quads
-// 	donut();		// !STUDENT_DONOT_SEE    uses virtual timer for animation
-	/* STUDENT_TODO: your code here */
 
-	// quest: pixel donut. call donut_simple()
-	/* to enable it,  irq handler must be modified to call sys_timer_irq_simple() */
-// 	// donut_simple();		// !STUDENT_DONOT_SEE		directly uses hw timer irq for animation
-	/* STUDENT_TODO: your code here */
-	
-	// donut_text();		// textual UART donut animation
+	// Quest 08 disabled so execution can continue
+	// test_fb_voffset();              // cycle through color quads
+
+	// donut();                        // uses virtual timer for animation
+
+	// Quest 10: pixel donut
+	// donut_simple();
+
+	// donut_text();                   // textual UART donut animation
 
 	while (1)
-		asm volatile("wfi");            // what happen here?
+		asm volatile("wfi");
 }
